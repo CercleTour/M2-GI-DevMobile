a0c9b631a7aaa780388adf9ecaca3cc5ecc3cd05
pepp3rrr <max.fro2021@gmail.com>
2026-03-31 17:44:10 +0200

    feat(app): implement deep linking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,25 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router, private zone: NgZone) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      this.zone.run(() => {
+        try {
+          const url = new URL(event.url);
+          if (url.hostname.toLowerCase() === 'cercletour.github.io') {
+            const appPath = url.pathname;
+            if (appPath) {
+              this.router.navigateByUrl(appPath);
+            }
+          }
+        } catch (e) {
+          console.error('Invalid URL received from appUrlOpen event', e);
+        }
+      });
+    });
+  }
 }
