769326dc937d416b4c9c022e41c369df158d97cc
pepp3rrr <max.fro2021@gmail.com>
2026-03-31 15:54:38 +0200

    feat: Add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thiscourseisascam.kahoot',
+  appName: 'kahoot',
+  webDir: 'www',
+};
+
+export default config;
