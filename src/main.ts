9ce5b26e8212e237e550b0e5b8c3bb69c8d5ddbe
Maxim Frolov <max.fro2021@gmail.com>
2026-04-01 12:03:12 +0200

    fix: Change routing strategy to path based and try fixing  android deeplinks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter, withPreloading, withHashLocation, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
@@ -18,7 +18,7 @@ bootstrapApplication(AppComponent, {
     provideFirestore(() => getFirestore()),
     provideZoneChangeDetection(),{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withHashLocation(), withPreloading(PreloadAllModules)),
     provideAuth(() => getAuth()),
   ],
 });
